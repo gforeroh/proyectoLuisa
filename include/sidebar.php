25ea0c36bf35a57619fb43e31d59d318f8acdea5
Gerardo forero <gforeroh@gmail.com>
2019-09-10 21:07:02 -0500

    proyecto educatico

---
+++ include/sidebar.php
@@ -0,0 +1,42 @@
+<div id="sidebar">
+	<div class="inner">
+		<!-- Menu -->
+		<nav id="menu">
+			<header class="major">
+				<h2>Menu</h2>
+			</header>
+			<ul>
+				<li><a href="index.php">Homepage</a></li>
+				<li><a href="generic.php">Generic</a></li>
+				<li><a href="elements.php">Elements</a></li>
+				<li>
+					<span class="opener">Submenu</span>
+					<ul>
+						<li><a href="#">Lorem Dolor</a></li>
+						<li><a href="#">Ipsum Adipiscing</a></li>
+						<li><a href="#">Tempus Magna</a></li>
+						<li><a href="#">Feugiat Veroeros</a></li>
+					</ul>
+				</li>
+				<li><a href="#">Etiam Dolore</a></li>
+				<li><a href="#">Adipiscing</a></li>
+				<li>
+					<span class="opener">Another Submenu</span>
+					<ul>
+						<li><a href="#">Lorem Dolor</a></li>
+						<li><a href="#">Ipsum Adipiscing</a></li>
+						<li><a href="#">Tempus Magna</a></li>
+						<li><a href="#">Feugiat Veroeros</a></li>
+					</ul>
+				</li>
+				<li><a href="#">Maximus Erat</a></li>
+				<li><a href="#">Sapien Mauris</a></li>
+				<li><a href="#">Amet Lacinia</a></li>
+			</ul>
+		</nav>
+		<!-- Footer -->
+		<footer id="footer">
+			<p class="copyright">Proyecto Luisa Forero</p>
+		</footer>
+	</div>
+</div>
